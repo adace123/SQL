6500533f5394ca8e32b53217be23f688c03e31c1
Aaron <rexforddrive@gmail.com>
2017-11-25 21:13:29 -0800

    Update join.sql

--- sqlzoo solutions/tutorials/join.sql
+++ sqlzoo solutions/tutorials/join.sql
@@ -60,3 +60,10 @@ WHERE teamid = 'GER'
 GROUP BY matchid, mdate 
 
 --13
+SELECT mdate, team1,
+SUM(CASE WHEN teamid = team1 THEN 1 ELSE 0 END) AS score1,
+team2,
+SUM(CASE WHEN teamid = team2 THEN 1 ELSE 0 END) AS score2
+FROM game LEFT JOIN goal ON matchid = id
+GROUP BY mdate, matchid, team1, team2
+ORDER BY mdate, matchid, team1, team2
